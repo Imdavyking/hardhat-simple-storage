4140bb6455cee2be3bd11d24d36ace34140896e6
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-07-29 14:26:50 +0100

    pushing to coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-rinkeby/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.8",
@@ -22,6 +26,15 @@ module.exports = {
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        gasPrice: 43,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
 }
 task("accounts", "Prints the list of accounts", async () => {
